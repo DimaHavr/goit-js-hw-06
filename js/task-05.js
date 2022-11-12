4b3ad6516b4675cd4ce0c0c7706a0088ed3ecec0
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2022-11-12 13:55:03 +0200

    update 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,8 @@
 const inputEl = document.querySelector("#name-input");
 const nameEl = document.querySelector("#name-output");
 
-inputEl.addEventListener("input", (event) => {
-  if (inputEl.value.trim().length === 0) {
-    nameEl.textContent = "Anonymous";
-  } else {
-    nameEl.textContent = inputEl.value.trim();
-  }
-});
+inputEl.addEventListener("input", (event) =>
+  inputEl.value.trim().length === 0
+    ? (nameEl.textContent = "Anonymous")
+    : (nameEl.textContent = inputEl.value.trim())
+);
